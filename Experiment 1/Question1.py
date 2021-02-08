11b3240e5b2a727d673f7d81c48fc35b72ac46cd
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-08 12:34:09 +0530

    Update Question1.py

--- Experiment 1/Question1.py
+++ Experiment 1/Question1.py
@@ -4,9 +4,9 @@ stop_words = stop_words_string.split()
 print(stop_words)
 
 paragraph = "Ronaldo made his senior international debut for Portugal in 2003 at age 18 , and has since earned 170 caps , including appearing and scoring in ten major tournaments , becoming Portugal's most capped player and his country's all-time top goalscorer. He scored his first international goal at Euro 2004 where he helped Portugal reach the final and assumed full captaincy of the national team in July 2008. In 2015 Ronaldo was named the best Portuguese player of all time by the Portuguese Football Federation. The following year he led Portugal to their first triumph in a major tournament by winning Euro 2016, and received the Silver Boot as the second-highest goalscorer of the tournament."
-result = []
+result = ""
 paragraph = paragraph.split()
 for word in paragraph:
   if word not in stop_words:
-    result.append(word)
+    result = result + word + " "
 print(result)
